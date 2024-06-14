5d9b96275a2b13adaa5bf77791f6f2137f7155a0
jdav892 <jadavila9@gmail.com>
2024-06-13 23:33:05 -0400

    finished object with methods and comments

--- jitLet.js
+++ jitLet.js
@@ -899,6 +899,75 @@ const workingCopy = {
     }
 };
 
+const config = {
+    isBare: function(){
+// returns true if the repository is bare
+        return config.read().core[""].bare === "true";
+    },
+
+    assertNotBare: function(){
+// throws if the repository is bare
+        if(config.isBare()){
+            throw new Error("this operation must be run in a work tree");
+        }
+    },
+
+    read: function(){
+// returns the contents of the config file as a nested JS object
+        return config.strToObj(files.read(files.jitletPath("config")));
+    },
+
+    write: function(configObj){
+// stringifies the nested JS object configObj and overwrites the config file with it
+        files.write(files.jitletPath("config"), config.objToStr(configObj));
+    },
+
+    strToObj: function(str){
+// parses the config string str and returns its contents as a nested JS object
+        return str.split("[")
+            .map(function(item) { return item.trim(); })
+            .filter(function(item) { return item !== ""; })
+            .reduce(function(c, item){
+                const lines = item.split("\n");
+                const entry = [];
+
+                entry.push(lines[0].match(/([^ \]]+)( |\])/)[1]);
+
+                const subsectionMatch = lines[0].match(/\"(.+)\"/);
+                const subsection = subsectionMatch === null ? "" : subsectionMatch[1];
+                entry.push(subsection);
+
+                entry.push(lines.slice(1).reduce(function(s, l){
+                    s[l.split("=")[0].trim()] = l.split("=")[1].trim();
+                    return s;
+                }, {}));
+                return util.setIn(c, entry);
+            }, { "remote" : {} });
+    }, 
+
+    objToStr: function(configObj){
+// configObj is a JS object that holds the config for the repository
+        return Object.keys(configObj)
+        .reduce(function(arr, section) {
+            return arr.concat(
+                Object.keys(configObj[section])
+                .map(function(subsection){ return { section: section, subsection: subsection}})
+            );
+        }, [])
+        .map(function(entry){
+            const subsection = entry.subsection === "" ? "" : " \"" + entry.subsection +"\"";
+            const settings = configObj[entry.section][entry.subsection];
+            return "[" + entry.section + subsection + "]\n" +
+            Object.keys(settings)
+            .map(function(k) { return "  " + k + " = " + settings[k]; })
+            .join("\n") + "\n";
+        })
+        .join("");
+
+    }
+
+};
+
 
 
 
